6bbdf32712fcc85c3d5b9882c949bc89c7d41539
yamoyamoto <sf22588f@st.omu.ac.jp>
2023-11-02 14:06:53 +0900

    add: page iterator

--- storage/storage.go
+++ storage/storage.go
@@ -9,3 +9,31 @@ func NewStorage(diskManager *DiskManager) *Storage {
 		DiskManager: diskManager,
 	}
 }
+
+type PageIterator struct {
+	diskManager *DiskManager
+	tableName   string
+	pageId      uint64
+
+	Page *Page
+}
+
+func (st *Storage) NewPageIterator(tableName string) *PageIterator {
+	return &PageIterator{
+		diskManager: st.DiskManager,
+		tableName:   tableName,
+		pageId:      0,
+	}
+}
+
+func (it *PageIterator) Next() bool {
+	p, err := it.diskManager.ReadPage(it.tableName, it.pageId)
+	// TODO: add page not found case
+	if err != nil {
+		return false
+	}
+
+	it.pageId++
+	it.Page = p
+	return true
+}
